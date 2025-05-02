876dab35396e5e2097b9134ee13c2c20c4242689
KC Ng <nkc900201@gmail.com>
2025-05-02 22:33:25 +0900

    feat: add to-do list

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,3 +1,10 @@
+/* To-do
+ * Implement design components
+ * Implement form check for email
+ * Set up testing
+ * Implement form submission handling
+ */
+
 function App() {
   return <div className="relative overflow-hidden bg-white">Hello World!</div>
 }
