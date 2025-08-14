8ad58e5a32b2569e1dfd6ca5eebc932d40204347
KC Ng <nkc900201@gmail.com>
2025-08-14 12:21:13 +0900

    feat: remove unused component, update SEO and metatags

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -18,7 +18,7 @@ import pages from "@/utils/pages"
  *  [x] Form checking for email input
  * [ ] Amend mobile layout design for signup form
  * [ ] Implement component and unit testing
- * [ ] Set up extra page
+ * [x] Set up extra page
  *  [x] Submission success page **
  * [x] Set up mobile view
  * [ ] Improve the SEO, meta-tags of website
